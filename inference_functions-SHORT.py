9778b054a962d52d1448b5c7ad2c9cb23625cd37
Mark Kramer <mak@bu.edu>
2024-10-04 16:51:52 -0400

    Create inference_functions-SHORT.py

---
+++ inference_functions-SHORT.py
@@ -0,0 +1,36 @@
+import scipy.io as sio
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
+import statsmodels.api as sm
+
+def load_data():
+    import pandas as pd
+    df = pd.read_csv("https://raw.githubusercontent.com/Mark-Kramer/METER-Units/master/swim_lesson_data.csv")
+    swim_lessons = np.array(df.iloc[0:N-1,0])
+    drownings    = np.array(df.iloc[0:N-1,1])
+    xy           = np.array(df.iloc[0:N-1,2])
+    return swim_lessons,drownings,xy
+
+def load_more_data():
+    import pandas as pd
+    df = pd.read_csv("https://raw.githubusercontent.com/Mark-Kramer/METER-Units/master/swim_lesson_data.csv")
+    swim_lessons        = np.array(df.iloc[0:N-1,0])
+    drownings           = np.array(df.iloc[0:N-1,1])
+    xy                  = np.array(df.iloc[0:N-1,2])
+    distance_from_ocean = np.array(df.iloc[0:N-1,3])
+    return swim_lessons,drownings,xy
+
+def compute_residuals_2d(swim_lessons, drownings):    
+    from statsmodels.formula.api import ols                    # import the required module
+    dat                = {"x": swim_lessons, "y": drownings}   # define the predictor "x" and outcome "y"
+    regression_results = ols("y ~ 1 + x", data=dat).fit()      # fit the model.
+    residuals = regression_results.resid
+    return residuals
+
+def compute_residuals_3d(swim_lessons, drownings, distance_from_ocean):    
+    from statsmodels.formula.api import ols                    # import the required module
+    dat = {"w": distance_from_ocean, "x": swim_lessons, "y": drownings}
+    regression_results = ols("y ~1 + w + x", data=dat).fit()
+    residuals = regression_results.resid
+    return residuals
