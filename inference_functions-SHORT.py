09162ed7868227adbce0cc7695e48e9a446d5cbd
Mark-K <mak@bu.edu>
2025-01-13 09:51:19 -0500

    Update inference_functions-SHORT.py

--- inference_functions-SHORT.py
+++ inference_functions-SHORT.py
@@ -13,14 +13,14 @@ def load_data():
     y            = np.array(df.iloc[:,3])
     return swim_lessons,drownings,x,y
 
-def load_more_data():
-    import pandas as pd
-    df = pd.read_csv("https://raw.githubusercontent.com/Mark-Kramer/METER-Units/master/swim_lesson_data.csv")
-    swim_lessons        = np.array(df.iloc[:,0])
-    drownings           = np.array(df.iloc[:,1])
-    xy                  = np.array(df.iloc[:,2])
-    distance_from_ocean = np.array(df.iloc[:,3])
-    return swim_lessons,drownings,x,y,distance_from_ocean
+#def load_more_data():
+#    import pandas as pd
+#    df = pd.read_csv("https://raw.githubusercontent.com/Mark-Kramer/METER-Units/master/swim_lesson_data.csv")
+#    swim_lessons        = np.array(df.iloc[:,0])
+#    drownings           = np.array(df.iloc[:,1])
+#    xy                  = np.array(df.iloc[:,2])
+#    distance_from_ocean = np.array(df.iloc[:,3])
+#    return swim_lessons,drownings,x,y,distance_from_ocean
 
 def compute_residuals_2d(swim_lessons, drownings):    
     from statsmodels.formula.api import ols                    # import the required module
@@ -36,12 +36,12 @@ def compute_residuals_3d(swim_lessons, drownings, distance_from_ocean):
     residuals = regression_results.resid
     return residuals
 
-def plot_spatial_coordinates(xy, colors):
+def plot_spatial_coordinates(x, y, colors):
     #import plotly.graph_objects as go
 
     # Example x-y coordinates
-    x_coordinates = xy[:,1]
-    y_coordinates = xy[:,0]
+    x_coordinates = x
+    y_coordinates = y
     # Create a scattermapbox trace
     trace = go.Scattermapbox(
         lat=y_coordinates,
