b9e3a51cfabb4f610c609bc6089c6099528044b5
Mark Kramer <mak@bu.edu>
2025-08-06 10:03:39 -0400

    Update pvalue_functions-SHORT.py

--- pvalue_functions-SHORT.py
+++ pvalue_functions-SHORT.py
@@ -14,7 +14,16 @@ def load_data():
     df = pd.read_csv("https://raw.githubusercontent.com/Mark-Kramer/METER-Units/master/spindle_data_baseline.csv", header=None)
     baseline = df.to_numpy()
     baseline = baseline[0]
-    return baseline
+
+    df = pd.read_csv("https://raw.githubusercontent.com/Mark-Kramer/METER-Units/master/spindle_data_during_treatment.csv", header=None)
+    during = df.to_numpy()
+    during = during[0]
+
+    df = pd.read_csv("https://raw.githubusercontent.com/Mark-Kramer/METER-Units/master/spindle_data_post_treatment.csv", header=None)
+    post = df.to_numpy()
+    post = post[0]
+    
+    return baseline, during, post
 
 def load_data(using_colab=0):
     # Load the data.
