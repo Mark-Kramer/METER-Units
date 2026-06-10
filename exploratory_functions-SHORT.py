63069171d335e85c2a63d4dc8a9c667bd2d8bc3f
Mark Kramer <mak@bu.edu>
2026-06-10 15:03:04 -0400

    Update exploratory_functions-SHORT.py

--- exploratory_functions-SHORT.py
+++ exploratory_functions-SHORT.py
@@ -13,3 +13,20 @@ def load_data():
 def plot_spike_train(t, spikes):
     indices = [i for i, value in enumerate(spikes) if value == 1]; values  = [1] * len(indices)
     plt.plot(t[indices], values, 'ko');
+
+def compute_p_values(spikes, signals):
+    n1 = signals.shape[1]
+    n2 = spikes.shape[1]
+    p = np.zeros((n1, n2))
+
+    for i in tqdm(range(n1)):       # tqdm shows the progress bar
+        for j in range(n2):
+                                    # GLM fitting with a Poisson family
+            X = signals[:, i]       # Predictor variable
+            y = spikes[:, j]        # Response variable
+            X = sm.add_constant(X)  # Adding a constant column for the intercept
+            glm_model = sm.GLM(y, X, family=sm.families.Poisson())
+            glm_results = glm_model.fit()
+            p[i, j] = glm_results.pvalues[1]  # Storing the p-value of the predictor
+
+    return p
